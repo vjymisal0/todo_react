c0a9a0daae89888b0a488b8431161e7c051b5f18
Vijay Misal <misalvijay153@gmail.com>
2024-05-23 13:06:12 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -67,6 +67,6 @@ app.put("/update-todo/:id", async (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
 
